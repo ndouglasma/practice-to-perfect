0f6894b94e18908231a96c26c99cb1ac2f6b29d4
Natalie Douglas <ndouglasma@gmail.com>
2018-05-10 11:23:31 -0400

    Resolve rspec failure due to non-existence of spec/support/valid_attribute.rb file.

---
+++ spec/controllers/api/v1/question_categories_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::QuestionCategoriesController, type: :controller do
+  describe "GET#index" do
+    it "should return a list of all the question categories" do
+      expect(true).to eq(true)
+    end
+  end
+end
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,12 @@
+require 'factory_bot'
+
+FactoryBot.define do
+  # factory :user do
+  #   sequence(:email) {|n| "user#{n}@example.com" }
+  #   username 'testuser'
+  #   first_name 'test'
+  #   last_name 'user'
+  #   password 'password'
+  #   password_confirmation 'password'
+  # end
+end
