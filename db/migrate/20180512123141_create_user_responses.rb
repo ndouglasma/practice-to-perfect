4b17352a61dfed546fb571fc817392f474a15c49
Natalie Douglas <ndouglasma@gmail.com>
2018-05-16 11:49:07 -0400

    Cleanup for Heroku deployment.

--- db/migrate/20180512123141_create_user_responses.rb
+++ db/migrate/20180512123141_create_user_responses.rb
@@ -10,7 +10,7 @@ class CreateUserResponses < ActiveRecord::Migration[5.2]
       t.time :audio_start_time
       t.time :audio_stop_time
       t.string :aws_transcribe_job_name
-      t.string :aws_transcribe_transcript_file_uri
+      t.text :aws_transcribe_transcript_file_uri
       t.time :aws_transcribe_creation_time
       t.time :aws_transcribe_completion_time
       t.string :aws_transcribe_job_status, default: 'NOT_STARTED'
