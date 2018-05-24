bd45048d1413066c91b7c01520656331f0fc2f3c
Natalie Douglas <ndouglasma@gmail.com>
2018-05-23 22:34:35 -0400

    Correct background image url so image will show.  Start process to sync github login with React front-end using JWT

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,21 @@
+require 'Auth'
+require 'auth_token'
+
 class SessionsController < ApplicationController
+  # skip_before_action :authenticate
+
   def create
-    auth = request.env["omniauth.auth"]
+    # auth = request.env["omniauth.auth"]
     user =  User.create_from_omniauth(auth)
 
     if user.persisted?
       user.update_from_omniauth(auth)
       session[:user_id] = user.id
 
-      redirect_to root_url
-      flash[:notice] = "You are now signed in!"
+      jwt = Auth.encode_id(user.id)
+      redirect_to(ENV['DIVE_LOG_CLIENT_URL'] + "?token=#{jwt}")
+      # redirect_to root_url
+      # flash[:notice] = "You are now signed in!"
     end
   end
 
@@ -17,5 +24,11 @@ class SessionsController < ApplicationController
     @current_user = nil
     redirect_to root_url
     flash[:notice] = "Signed Out!"
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
