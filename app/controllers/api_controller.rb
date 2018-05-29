15ef4055dfb847eb26c82021090989c499c92529
Natalie Douglas <ndouglasma@gmail.com>
2018-05-29 15:39:33 -0400

    Comment out authenticate check in ApiController since not fully functional.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,5 @@
 class ApiController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery unless: -> { request.format.json? }
-  before_action :authenticate
+  # before_action :authenticate
 end
